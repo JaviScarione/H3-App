a25ae7cb25fa297037c9e09205739883b619d46d
Javier <javierscarione@gmail.com>
2023-01-24 23:56:24 -0300

    Prepare App

---
+++ src/components/MainComponent.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Routes, Route } from 'react-router-dom';
+
+
+function Main() {
+
+    return (
+      <div>
+        <Routes>
+          <Route path="/" />
+        </Routes>
+      </div>
+    );
+  }
+  
+    
+export default Main;
