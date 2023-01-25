9f6393120a858b3f75e5d3327672ae3e2cb898e8
Javier <javierscarione@gmail.com>
2023-01-25 01:11:28 -0300

    login front without connect

--- src/components/LogInComponent.js
+++ src/components/LogInComponent.js
@@ -1,55 +1,42 @@
-import React, { Component} from "react";
-import { Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem, Jumbotron,
-    Button, Modal, ModalHeader, ModalBody, Form, FormGroup, Input, Label } from 'reactstrap';
+import React, { useState } from "react";
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-regular-svg-icons';
 import '../css/Login.css';
 
 
 
-class Login extends Component {
+function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-    constructor(props) {
-        super(props);
-
-        this.handleLogin = this.handleLogin.bind(this);   
-    }
-    
+       
           
-      handleLogin(event) {
-        alert("Usuario: " + this.username.value + " Contraseña: " + this.password.value);
-        event.preventDefault();
+    function handleLogin(event) {
+      event.preventDefault();
+      // Validate form and send data to server
     }
 
-    render() {
+     
         return(
-            <div className="wrapper">
-                <div className="text-center mt-4 name">
-                    Bienvenido!
-                </div>
-                <div className="text-center credentials">
-                    Ingresa tus credenciales
-                </div>
-                <Form className="p-3 mt-3" onSubmit={this.handleLogin}>
-                    <FormGroup>
-                        <div className="form-field d-flex align-items-center">
-                            <FontAwesomeIcon icon={faUser} />
-                            <input type="text" name="Mail" id="Mail" placeholder="Email"/>
-                        </div>
-                    </FormGroup>
-                    <FormGroup>
-                        <div className="form-field d-flex align-items-center">
-                            <FontAwesomeIcon icon="fa-solid fa-key" />
-                            <input type="password" name="password" id="pwd" placeholder="Contraseña"/>
-                        </div>
-                    </FormGroup>
-                    <a href="/">
-                        <Button type="submit" value="submit"className="btn mt-3">Ingresar</Button>
-                    </a>                
-                </Form>        
-            </div>
+            <Form className="wrapper" onSubmit={handleLogin}>
+                <FormGroup className="text-center my-1 name">
+                    <Label>Bienvenido!</Label>
+                </FormGroup>
+                <FormGroup className="text-center credentials">
+                    <Label>Ingresa tus credenciales</Label>
+                </FormGroup>                    
+                <FormGroup className="form-field d-flex align-items-center">
+                        <FontAwesomeIcon icon={faUser} />
+                        <Input type="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="Email"/>                
+                </FormGroup>
+                <FormGroup className="form-field d-flex align-items-center">
+                        <FontAwesomeIcon icon="fa-solid fa-key" />
+                        <Input type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="Contraseña"/>
+                </FormGroup>
+                <Button type="submit" value="submit"className="btn mt-3">Ingresar</Button>
+            </Form>        
         );
     }
-}
 
 export default Login;
